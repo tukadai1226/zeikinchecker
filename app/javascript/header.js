728598bc8e93f508ee0bafbd740712f8a22359a5
tukadai1226 <tukadai1226@icloud.com>
2023-11-15 23:52:32 +0900

    ヘッダーのアニメーションの実装

--- app/javascript/header.js
+++ app/javascript/header.js
@@ -1,9 +1,16 @@
+const header = () => {
 const CLASSNAME = "-visible";
 const TIMEOUT = 1000;
 const $target = $(".title");
+
 
 setInterval(() => {
   $target.addClass(CLASSNAME);
 }, TIMEOUT * 2);
 
 
+
+
+};
+window.addEventListener("turbo:load", header);
+window.addEventListener("turbo:render", header);
