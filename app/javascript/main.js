cd1e78482507ecc5bc24fc1d7e321a45868ce72b
tukadai1226 <tukadai1226@icloud.com>
2023-11-15 23:54:32 +0900

    メイン部分のリダイレクト時の仕様実装

--- app/javascript/main.js
+++ app/javascript/main.js
@@ -1,3 +1,4 @@
+const main = () => {
 const CLASSNAME = "-visible";
 const TIMEOUT = 2000;
 const $target = $(".bg");
@@ -6,4 +7,6 @@ let animationStopped = false;
 setTimeout(() => {
   $target.addClass(CLASSNAME); // アニメーションを開始
 }, TIMEOUT);
-
+};
+window.addEventListener("turbo:load", main);
+window.addEventListener("turbo:render", main);
